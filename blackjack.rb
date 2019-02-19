56ba910131c2ffea99e43be9b032c65abac30b58
Kirk Burleson <kirk.burleson@yahoo.com>
2019-02-19 11:48:40 -0600

    Basic flow is working. Hit and Stand work. Swaps aces from 11 to 1 when needed.

--- blackjack.rb
+++ blackjack.rb
@@ -1,27 +1,14 @@
 class Game
 
-class Test
-	def initialize
-		@hand = []
-	end
-
-	def total
-		first = @hand[0].to_int
-	end
-end
-
 def initialize num_decks = 1
-	cards = [	2,3,4,5,6,7,8,9,10,'J','Q','K','A',
-				2,3,4,5,6,7,8,9,10,'J','Q','K','A',
-				2,3,4,5,6,7,8,9,10,'J','Q','K','A',
-				2,3,4,5,6,7,8,9,10,'J','Q','K','A']
-	@shoe = cards * num_decks
+	cards = [2,3,4,5,6,7,8,9,10,'J','Q','K','A']
+	suits = 4
+	@shoe = (cards * suits) * num_decks
 	@playing = true
-	@choice = nil
-	@flip = false
 	@next = -1
-	@player = []
+	@player = [['A',3],['5','K'],['A','10']]
 	@dealer = []
+	@bet = 0
 end
 
 
@@ -37,7 +24,7 @@ def shuffle
 end
 
 def next_card
-	if @next == (@shoe.length - 1)
+	if @next >= (@shoe.length - 8)
 		@next = -1
 		shuffle
 	end
@@ -46,53 +33,99 @@ def next_card
 	@shoe[@next]
 end
 
-def deal
-	@player.push(next_card)
-	@player.push(next_card)
-	@dealer.push(next_card)
-	@dealer.push(next_card)
-end
-
-
-def play
-	@playing = false
-end
-
-
-def show_cards
-	print "\n"*5
-
-	puts "Dealer: #{@dealer.join ' '} --> #{total @dealer}"
-	puts "Player: #{@player.join ' '} --> #{total @player}"
+def get_card_value card
+	if card == 'J' || card == 'Q' || card == 'K'
+		10
+	elsif card == 'A'
+		11
+	else
+		card.to_i
+	end
 end
 
 def total hand
 	total = 0
-	hand.each do |rank|
-		total = total + get_card_value(rank)
+	hand.each do |card|
+		total = total + get_card_value(card)
+		if total > 21
+			# use ace as 1 instead of 11
+			has_ace = hand.index('A')
+			if has_ace != nil
+				hand[has_ace] = 1
+				total = total - 10
+			end
+		end
 	end
+
+	if total == 21 && hand.length == 2
+		total = 'BLACKJACK'
+	elsif total > 21
+		total = 'BUST'
+	end
+
 	total
 end
-
-def get_card_value card
-	if card == 'J' || card == 'Q' || card == 'K'
-		10
-	else
-		if card == 'A'
-			11
-		else
-			card.to_i
-		end		
-	end
-end
-
 
 def start
 	shuffle
 	while @playing == true
-		deal
-		show_cards
-		play
+		print "Bet: "
+		@bet = gets.chomp.to_i
+		if @bet < 1 then next end
+
+		@player.push([next_card, next_card])
+		@dealer.push(next_card)
+		@dealer.push('?')
+		
+		puts '******************'
+		puts "Dealer: #{@dealer.join ' '}"
+
+		# loop through each hand, one at a time.
+		current_hand = 0
+		@player.each do |hand|
+			current_hand = current_hand + 1
+
+			# loop until finished with this hand
+			while true
+				# show previous hands
+				prev_hand = 0
+				while prev_hand < current_hand
+					# show swapped aces as 'A' instead of 1
+					tmp_hand = @player[prev_hand].collect {|x| x == 1 ? 'A' : x}
+					puts "Hand #{prev_hand + 1}: #{tmp_hand.join ' '} #{'----'*current_hand}> #{total @player[prev_hand]}"
+					prev_hand = prev_hand + 1
+				end
+				
+				print "\n(H)it (S)tand (D)ouble Down s(P)lit (Q)uit >>"
+				command = gets.chomp.downcase
+
+				if command == 'h'
+					hand.push(next_card)
+				elsif command == 's'
+					break
+				elsif command == 'd'
+					
+				elsif command == 'p'
+
+				elsif command == 'q'
+					Process.exit
+				end
+
+				if (total hand) == 'BUST' then break end
+			end
+		end
+
+		# show players final scores
+		i = 0
+		@player.each do |hand|
+			i = i + 1
+			puts "Hand #{i + 1}: #{hand.join ' '} #{'----'*i}> #{total hand}"
+		end
+
+		# Dealers turn
+		puts
+		
+
 	end
 end
 end
